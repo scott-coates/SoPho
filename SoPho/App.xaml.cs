ffa6e58a929e4a2fe0e69d32f20eff66a66656e2
ScottC <devnull@localhost>
2011-07-27 01:20:11 -0700

    added console hybrid

--- SoPho/App.xaml.cs
+++ SoPho/App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Threading;
@@ -14,14 +16,62 @@ namespace SoPho
     /// </summary>
     public partial class App : Application
     {
+        //http://www.rootsilver.com/2007/08/how-to-create-a-consolewindow
+        //http://stackoverflow.com/questions/426421/wpf-command-line
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool AttachConsole(int dwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool FreeConsole();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out int lpdwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool AllocConsole();
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
             if (e.Args.Contains("auto"))
             {
-                var task = new MainWindow().DownloadPhotos();
-                WaitWithPumping(task);
+                try
+                {
+                    //Get a pointer to the forground window.  The idea here is that
+                    //IF the user is starting our application from an existing console
+                    //shell, that shell will be the uppermost window.  We'll get it
+                    //and attach to it
+                    IntPtr ptr = GetForegroundWindow();
+
+                    int u;
+
+                    GetWindowThreadProcessId(ptr, out u);
+
+                    Process process = Process.GetProcessById(u);
+
+                    if (process.ProcessName == "cmd") //Is the uppermost window a cmd process?
+                    {
+                        AttachConsole(process.Id);
+                    }
+                    else
+                    {
+                        //no console AND we're in console mode ... create a new console.
+
+                        AllocConsole();
+                    }
+                    var task = new MainWindow().DownloadPhotos();
+                    WaitWithPumping(task);
+                    Console.Read();
+                }
+                finally
+                {
+                    FreeConsole();
+                }
             }
             else
             {
@@ -35,7 +85,8 @@ namespace SoPho
             if (task == null) throw new ArgumentNullException("task");
             var nestedFrame = new DispatcherFrame();
             task.ContinueWith(_ => nestedFrame.Continue = false);
-            Dispatcher.PushFrame(nestedFrame); //execute this loop until all other tasks are done. it won't block the ui thread
+            Dispatcher.PushFrame(nestedFrame);
+                //execute this loop until all other tasks are done. it won't block the ui thread
             task.Wait();
         }
     }
